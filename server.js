6ec004526abc33468c727a0f0d9bbbfd18df1381
paulrajswetha <paulswetha20@gmail.com>
2026-06-14 10:12:20 +0530

    first commit

--- server.js
+++ server.js
@@ -23,12 +23,21 @@ const LANGUAGE_NAMES = {
   java:       'Java',
 };
 
-app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:5174','http://localhost:3000','http://127.0.0.1:5173','https://logistack0.netlify.app/'],
-  credentials: true,
+
+
+
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "https://logistack0.netlify.app",
+    credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
-}));
+  })
+);
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => { console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`); next(); });
