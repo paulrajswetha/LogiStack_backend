fb524bffc9e2abffb86fae657f92a3683063770d
paulrajswetha <paulswetha20@gmail.com>
2026-06-14 10:01:58 +0530

    first commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const LANGUAGE_NAMES = {
 };
 
 app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:5174','http://localhost:3000','http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173','http://localhost:5174','http://localhost:3000','http://127.0.0.1:5173','https://logistack0.netlify.app/'],
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
